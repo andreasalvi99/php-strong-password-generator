f133f29ac4603073da085286f340ce31f22b2e1c
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-30 12:48:26 +0200

    set for loop and rand method for randomic generator

--- password.php
+++ password.php
@@ -1,7 +1,38 @@
-<?php 
+<?php
 
-function generate_password(string $length) {
-    return "La tua password sarà di $length caratteri";
+function generate_password($length) {
+
+//^ Inizializzo variabili con caratteri minuscoli, maiuscoli, numeri e speciali
+
+    $lowercase = "abcdefghijklmnopqrstuvwxyz";
+    $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $numbers = "1234567890";
+    $simbols = "!@#$%^&*()_+-";
+
+//^ Concateno le stringhe
+
+    $all_chars = $lowercase . $uppercase . $numbers . $simbols;
+
+//^ Definisco la variabile password come strimga vuota che andrà riempita di volta in volta con un ciclo
+
+    $password = "";
+
+//^ Apro il cliclo che si ripete fino al numero di volte indicato dall'input dell'utente
+
+    for($i = 0; $i <= $length; $i++) {
+
+    //^ Uso funzione rand per estrarre randomicamente ad ogni ciclo un carattere dalla strimga unica
+
+        $random_char = $all_chars[rand(0, strlen($all_chars) -1 )];
+
+    //^ Ad ogni ciclo prendo il carattere estratto e lo concateno in password
+
+        $password .= $random_char;
+    };
+
+//^ Ritorno la password
+
+    return $password;
 };
 
 ?>
@@ -16,7 +47,7 @@ function generate_password(string $length) {
 </head>
 <body>
 
-<h1>Ecco la tua password: <?php echo generate_password($_GET["password-length"])?></h1>
+<h1>Ecco la tua password: <?php echo generate_password((int) $_GET["password-length"])?></h1>
     
 </body>
 </html>
